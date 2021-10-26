5a6cd630a53f9d9bce017d9a77cd7c35ad38f4dd
DanielDuplat <ddaniel1912@gmail.com>
2021-10-26 12:10:47 -0300

    Fixing static assets location

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -7,12 +7,12 @@
 	<main>
 		<div
 			class="hero min-h-screen"
-			style="background-image: url('./src/assets/home_background.jpg');"
+			style="background-image: url('./home_background.jpg');"
 		>
 			<div class="hero-overlay bg-opacity-60" />
 			<div class="text-center hero-content text-neutral-content">
 				<div class="max-w-md">
-					<h1 class="mb-5 text-5xl font-bold">Hello there!</h1>
+					<h1 class="mb-5 text-5xl font-bold">Hello there</h1>
 					<p class="mb-5">
 						Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
 						excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
